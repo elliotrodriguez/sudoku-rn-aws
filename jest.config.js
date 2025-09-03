f861da65387437d44dc1dfacc5409989c794acc7
Elliot Rodriguez <elliotrodriguez72@gmail.com>
2025-09-03 17:01:10 -0400

    initial commit/setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testMatch: [
+    '**/__tests__/**/*.(ts|tsx|js)',
+    '**/*.(test|spec).(ts|tsx|js)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
